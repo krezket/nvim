4287b1c35f587258abdfef2cf9ea62e10f4ded59
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-08-28 07:03:19 -0700

    added new remaps

--- lua/krezket/set.lua
+++ lua/krezket/set.lua
@@ -8,8 +8,12 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
 vim.opt.hlsearch = false
-vim.opt.incsearch = truea
+vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
