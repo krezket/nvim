57f56b93fbe71fae6e725af3000a62c896c5cc1b
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-11-20 16:49:21 -0800

    removed tab to esc reamap

--- lua/krezket/lazy/zenmode.lua
+++ lua/krezket/lazy/zenmode.lua
@@ -1,10 +1,10 @@
 return {
     "folke/zen-mode.nvim",
     config = function()
-        vim.keymap.set("n", "<leader>zz", function()
+        vim.keymap.set("n", "<leader>zZ", function()
             require("zen-mode").setup {
                 window = {
-                    width = 90,
+                    width = 150,
                     options = { }
                 },
             }
@@ -16,17 +16,17 @@ return {
         end)
 
 
-        vim.keymap.set("n", "<leader>zZ", function()
+        vim.keymap.set("n", "<leader>zz", function()
             require("zen-mode").setup {
                 window = {
-                    width = 80,
+                    width = 150,
                     options = { }
                 },
             }
             require("zen-mode").toggle()
             vim.wo.wrap = false
-            vim.wo.number = false
-            vim.wo.rnu = false
+            vim.wo.number = true
+            vim.wo.rnu = true
             vim.opt.colorcolumn = "0"
             ColorMyPencils()
         end)
--- lua/krezket/remap.lua
+++ lua/krezket/remap.lua
@@ -1,21 +1,25 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Moving Selected Lines in Visual Mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Joining Lines and Keeping the Cursor in Place
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Centering the Screen After Scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Centering the Screen After Searching
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Pasting Over Selected Text Without Yanking
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- Yanking to the System Clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
-
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "<C-f>", "<cmd>slient !tmux neww tmux-sessionizer<CR>")
