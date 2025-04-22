dca7f4915ea28775a270cd2e629f3029b3ca3218
Anthony Guerrero <anthonyguerrero62@outlook.com>
2025-04-21 18:18:33 -0700

    added tsx

--- lua/krezket/lazy/treesitter.lua
+++ lua/krezket/lazy/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "html", "javascript", "typescript", "c", "lua", "rust",
+                "tsx", "vimdoc", "html", "javascript", "typescript", "c", "lua", "rust",
                 "jsdoc", "bash",
             },
 
